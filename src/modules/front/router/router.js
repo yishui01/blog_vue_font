46e9f196f91b27c2d35ac2c2c0e46ea82846c183
yishui <727040917@qq.com>
2020-05-01 21:32:29 +0800

    去掉article路由懒加载，因为懒加载会导致首次跳转卡顿

--- src/modules/front/router/router.js
+++ src/modules/front/router/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Article from '../views/Article'
 Vue.use(Router)
 
 export default new Router({
@@ -21,7 +21,7 @@ export default new Router({
                 {
                     path: '/art',
                     name: 'art',
-                    component: () => import('../views/Article')
+                    component: Article
                 },
                 {
                     path: '/chat',
